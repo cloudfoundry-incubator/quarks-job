5f974bd77c838d15a2a95758c9836d6b680fce57
Mario Manno <mario.manno@suse.com>
2019-10-08 18:57:10 +0200

    Do not import converter

---
+++ pkg/kube/controllers/extendedjob/docker_image.go
@@ -0,0 +1,17 @@
+package extendedjob
+
+import "code.cloudfoundry.org/cf-operator/pkg/kube/util/names"
+
+// operatorDockerImage is the location of the operators own docker image
+var operatorDockerImage = ""
+
+// SetupOperatorDockerImage initializes the package scoped variable
+func SetupOperatorDockerImage(org, repo, tag string) (err error) {
+	operatorDockerImage, err = names.GetDockerSourceName(org, repo, tag)
+	return err
+}
+
+// GetOperatorDockerImage returns the image name of the operator docker image
+func GetOperatorDockerImage() string {
+	return operatorDockerImage
+}

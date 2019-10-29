09cae8aea23e685ca4af4da418909f345a20d947
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2019-10-29 16:45:58 +0100

    Fix command argument in exjob spec

--- pkg/kube/controllers/extendedjob/job_creator.go
+++ pkg/kube/controllers/extendedjob/job_creator.go
@@ -137,7 +137,7 @@ func (j jobCreatorImpl) Create(ctx context.Context, eJob ejv1.ExtendedJob, names
 		Name:            "output-persist",
 		Image:           config.GetOperatorDockerImage(),
 		ImagePullPolicy: config.GetOperatorImagePullPolicy(),
-		Args:            []string{"persist-output"},
+		Args:            []string{"util", "persist-output"},
 		Env: []corev1.EnvVar{
 			{
 				Name:  EnvNamespace,

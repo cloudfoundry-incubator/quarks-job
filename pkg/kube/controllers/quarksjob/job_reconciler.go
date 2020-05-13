3f8498dfe84090a253ea89c8e8e46d0f4844c388
Mario Manno <mario.manno@suse.com>
2020-05-13 16:14:38 +0200

    Limit quarks jobs list to a namespace

--- pkg/kube/controllers/quarksjob/job_reconciler.go
+++ pkg/kube/controllers/quarksjob/job_reconciler.go
@@ -112,7 +112,7 @@ func (r *ReconcileJob) Reconcile(request reconcile.Request) (reconcile.Result, e
 		qj.Status.Completed = true
 		err := r.client.Status().Update(ctx, &qj)
 		if err != nil {
-			ctxlog.WithEvent(&qj, "UpdateError").Errorf(ctx, "Failed to update quarks job status '%s' (%v): %s", qj.GetNamespacedName(), qj.ResourceVersion, err)
+			ctxlog.WithEvent(&qj, "UpdateError").Errorf(ctx, "Failed to update quarks job status '%s' (%s): %s", qj.GetNamespacedName(), qj.ResourceVersion, err)
 			return reconcile.Result{Requeue: false}, nil
 		}
 	}
